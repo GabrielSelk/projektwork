b5391410749da555582616d782cdaaf669097ff2
GabrielSelk <ev.gabor@gmail.com>
2023-11-22 18:25:49 +0100

    progress_save: step implementations language

--- src/test/java/steps/siteSetupSteps.java
+++ src/test/java/steps/siteSetupSteps.java
@@ -4,9 +4,7 @@ import browser.BrowserType;
 import browser.DriverInitializer;
 import browser.Settings;
 import io.cucumber.java.After;
-import io.cucumber.java.AfterAll;
 import io.cucumber.java.Before;
-import io.cucumber.java.BeforeAll;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
@@ -45,13 +43,15 @@ public class siteSetupSteps {
     @Then("The cookie pop-up disappears")
     public void theCookiePopUpDisappears() {
         By cookieButton = By.xpath("//button[text()='Elfogadom']");
-        ;
         List<WebElement> elements = driver.findElements(cookieButton);
         Assertions.assertTrue(elements.isEmpty() || !elements.get(0).isDisplayed(), "Cookie popup is still visible.");
     }
 
     @Given("the language is set to {string}")
     public void theLanguageIsSetTo(String arg0) {
+        By cookieButton = By.xpath("//button[text()='Elfogadom']");
+        driver.findElement(cookieButton).click();
+
         By title = By.tagName("h2");
         String titleText = driver.findElement(title).getText();
         Assertions.assertEquals("Utazástervezés", titleText, "The language is not hungarian.");
@@ -66,13 +66,19 @@ public class siteSetupSteps {
 
     @And("I change the language back to {string}")
     public void iChangeTheLanguageBackTo(String arg0) {
+
         By hungarianButton = By.xpath("//span[@title='magyar']");
         driver.findElement(hungarianButton).click();
     }
 
+    //Miért fail-el el? Talán túl gyors neki és még nincs betöltve az oldal? Beteszek egy wait-et.
     @Then("language is changed to <newPageLanguage>")
     public void languageIsChangedToNewPageLanguage() {
         By title = By.tagName("h2");
+
+        WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+        wait.until(ExpectedConditions.visibilityOfElementLocated(title));
+
         String titleText = driver.findElement(title).getText();
         Assertions.assertEquals("Utazástervezés", titleText, "The language is not hungarian.");
     }
